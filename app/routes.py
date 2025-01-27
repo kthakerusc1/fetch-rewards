32015420f4fe2fcc924965bd932e6d8137f85a58
KISHAN THAKER <60134668+kthakerusc1@users.noreply.github.com>
2025-01-27 17:48:32 -0500

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,21 @@
 from flask import request, jsonify
-from flask_restful import Api
-import uuid
-from app.models import in_memory_db
-from app.services import calculate_points
+from app.services import calculate_points, get_receipt_points
 
 def configure_routes(app):
-    api = Api(app)
 
     @app.route('/receipts/process', methods=['POST'])
     def process_receipt():
-        data = request.get_json()
-        receipt_id = str(uuid.uuid4())
-        points = calculate_points(data)
-        in_memory_db[receipt_id] = points
-        return jsonify({"id": receipt_id})
+        try:
+            data = request.get_json()
+            receipt_id = calculate_points(data)
+            return jsonify({"id": receipt_id})
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
 
     @app.route('/receipts/<receipt_id>/points', methods=['GET'])
     def get_points(receipt_id):
-        points = in_memory_db.get(receipt_id)
-        if points is None:
-            return jsonify({"error": "Receipt ID not found"}), 404
-        return jsonify({"points": points})
+        try:
+            points = get_receipt_points(receipt_id)
+            return jsonify({"points": points})
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
